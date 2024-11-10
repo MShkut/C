38d726eb053773cb1b2adec499d558d134c54288
Mitch <mitchshkut@proton.me>
2024-11-10 12:33:26 -0500

    First small additions to completing this assessment

--- Intro-to-Embedded-Systems/Module-1/stats.h
+++ Intro-to-Embedded-Systems/Module-1/stats.h
@@ -9,19 +9,45 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats 
+ * @brief stats analytics functions
  *
- * <Add Extended Description Here>
+ * From Coursera: "You are to write a couple of functions that can analyze an array of unsigned char data items and report analytics on the maximum, minimum, mean, and median of the data set. In addition, you will need to reorder this data set from large to small. All statistics should be rounded down to the nearest integer. After analysis and sorting is done, you will need to print that data to the screen in nicely formatted presentation. You will need to submit a version controlled repository of these 3 files."
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Mitch Shkut
+ * @date 2024-11-10
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
 /* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief print_statistics()
+ *
+ * A function that prints the statistics of an array including minimum, maximum, mean, and median
+ * Calls the individual min, max, mean, and median functions and returning their values
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+ 
+/**
+ * @brief print_array()
+ *
+ * Given an array of data and a length, prints the array to the screen.
+ * Takes the array and a length as inputs and returns a string of the array of the specified length
+ *
+ * @param <array[]> <The array defined in the main fuction>
+ * @param <len()> <The length of the array to print>
+ *
+ * @return <returns a print of the array, len() long>
+ */S
 
 /**
  * @brief <Add Brief Description of Function Here>
@@ -35,6 +61,4 @@
  *
  * @return <Add Return Informaiton here>
  */
-
-
 #endif /* __STATS_H__ */
